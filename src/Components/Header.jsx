9853c60000c784d27046aac11bffe3c46fdd5748
TatakAdi <tatakadisubagyo@gmail.com>
2024-10-29 16:24:18 +0800

    Pembuatan Component Header dan menghapus file usang

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import logoTest from "../assets/logoWebsite.png";
 import "../styles/Header.css";
 
-function Header() {}
+function Header() {
+  const navPelayanan = useState("Pelayanan");
+  const navBerita = useState("Berita");
+  const navRegulasi = useState("Regulasi");
+  const navData = useState("Data Statistik");
+  const navFAQ = useState("FAQ");
+  return (
+    <>
+      <header className="header">
+        <div className="Logo">
+          <div className="Judul">
+            <p id="subtitle">Pemerintah Kota</p>
+            <p id="title">Balikpapan</p>
+          </div>
+          <img src={logoTest} alt="Logo Website" id="Logo" />
+        </div>
+        <div className="NavBar">
+          <a href="" className="Nav">
+            {navPelayanan}
+          </a>
+          <a href="" className="Nav">
+            {navBerita}
+          </a>
+          <a href="" className="Nav">
+            {navRegulasi}
+          </a>
+          <a href="" className="Nav">
+            {navData}
+          </a>
+          <a href="" className="Nav">
+            {navFAQ}
+          </a>
+        </div>
+      </header>
+    </>
+  );
+}
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import App from "./Components/App.jsx";
+// import App from "./Components/App.jsx";
+import Header from "./Components/Header";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Header />
   </StrictMode>
 );
