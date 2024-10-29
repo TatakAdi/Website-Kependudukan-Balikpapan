12e36a2245c623d85ad76c8c823434d4f6aaea98
titohuda <ariffiantotito1@gmail.com>
2024-10-29 21:03:17 +0800

    halaman pelayanan

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./Components/App.jsx";
+import Pelayanan from "./Pelayanan.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Pelayanan />
   </StrictMode>
 );
