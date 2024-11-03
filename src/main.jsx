4dc49c69eceb54b589e18fb1c05f3e5c7d491776
titohuda <ariffiantotito1@gmail.com>
2024-11-03 22:04:35 +0800

    halaman berita-konten

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import Pelayanan from "./Pelayanan.jsx";
 import Header from "./Components/Header.jsx";
+import BeritaKonten from "./berita-konten.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
-    <Pelayanan />
+    <BeritaKonten />
   </StrictMode>
 );
