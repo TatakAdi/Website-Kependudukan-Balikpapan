f514eb91226762a76a2238562ee3f9531240f01d
TatakAdi <tatakadisubagyo@gmail.com>
2024-11-04 11:36:58 +0800

    Presentasi proggres 50% tubes desweb

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MainPage from "./LandingPage.jsx";
+import Pelayanan from "./Pelayanan.jsx";
+import BeritaKonten from "./berita-konten.jsx";
+import BeritaPage from "./PageBerita.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MainPage />
+    <BeritaKonten />
   </StrictMode>
 );
