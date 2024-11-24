84853b55d6fda5a8728eadf705cb4985dc48d1ee
TatakAdi <tatakadisubagyo@gmail.com>
2024-11-25 05:43:01 +0800

    Final konten for page regulasi

--- src/Components/RegulasiBox.jsx
+++ src/Components/RegulasiBox.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function RegulasiBox(props) {
   return (
-    <tr class="odd:bg-white even:bg-gray-50">
-      <td class="p-3 border border-gray-200">{props.JudulSurat}</td>
+    <tr className="odd:bg-white even:bg-gray-50">
+      <td className="p-3 border border-gray-200 text-purple-600 hover:text-purple-400 cursor-pointer">
+        <a href={props.link} target="_blank">
+          {props.JudulSurat}
+        </a>
+      </td>
     </tr>
   );
 }
