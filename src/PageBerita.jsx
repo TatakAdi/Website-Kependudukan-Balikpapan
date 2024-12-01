3c9180a00f6027a584ee44096d799d502324ca86
titohuda <ariffiantotito1@gmail.com>
2024-12-01 17:51:59 +0800

    responsive pageberita

--- src/PageBerita.jsx
+++ src/PageBerita.jsx
@@ -14,10 +14,13 @@ const cardData = beritaData.map((item) => ({
   fotoAdmin: item.fotoAdmin,
 }));
 
-
 const Card = ({ title, description, link, image, admin, fotoAdmin }) => (
-  <div className="card border border-gray-300 rounded-lg p-3 shadow-md bg-gray-200 text-left flex flex-col justify-between min-h-[250px]">
-    <img src={image} alt={title} className="w-full rounded-lg mb-3 h-40 object-cover" />
+  <div className="card border border-gray-300 rounded-lg p-3 shadow-md bg-gray-200 text-left flex flex-col justify-between min-h-[250px] md:min-h-[200px]">
+    <img
+      src={image}
+      alt={title}
+      className="w-full rounded-lg mb-3 h-40 object-cover"
+    />
     <h3 className="font-semibold text-lg mb-2">{title}</h3>
     <p className="text-sm text-gray-700">{description}</p>
     <div className="flex items-center mt-2">
@@ -36,7 +39,6 @@ const Card = ({ title, description, link, image, admin, fotoAdmin }) => (
     </Link>
   </div>
 );
-
 
 const BeritaPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -61,52 +63,55 @@ const BeritaPage = () => {
 
   return (
     <>
-    <div className="container mt-20 text-center text-gray-800">
-      <header>
-        <Header />
-      </header>
-      <main className="flex justify-center p-5 gap-1">
-        {/* Kartu Besar */}
-        <div className="flex flex-col justify-between w-full sm:w-3/5 p-3">
-          <Card {...currentItems[0]} />
-
-          {/* Pagination */}
-          <div className="mt-auto flex justify-center gap-2">
-            <button
-              className={`bg-gray-200 text-black py-2 px-4 rounded-lg shadow-md ${
-                currentPage === 1
-                  ? "opacity-50 cursor-not-allowed"
-                  : "hover:bg-gray-300"
-              }`}
-              onClick={prevPage}
-            >
-              ⇦ Sebelumnya
-            </button>
-            <button
-              className={`bg-gray-200 text-black py-2 px-4 rounded-lg shadow-md ${
-                currentPage === Math.ceil(cardData.length / itemsPerPage)
-                  ? "opacity-50 cursor-not-allowed"
-                  : "hover:bg-gray-300"
-              }`}
-              onClick={nextPage}
-              disabled={
-                currentPage === Math.ceil(cardData.length / itemsPerPage)
-              }
-            >
-              Selanjutnya ⇨
-            </button>
+      <div className="container mt-20 text-center text-gray-800">
+        <header>
+          <Header />
+        </header>
+        <main className="flex flex-col md:flex-col lg:flex-row justify-center p-5 gap-4">
+          {/* Kartu Besar */}
+          <div className="flex flex-col justify-between w-full lg:w-3/5 p-3">
+            <Card {...currentItems[0]} />
           </div>
-        </div>
 
-        {/* Kartu Kecil */}
-        <div className="grid grid-cols-2 mt-3 gap-4 w-full sm:w-3/5 md:w-2/3 lg:w-1/2">
-          {currentItems.slice(1).map((data, index) => (
-            <Card key={index} {...data} />
-          ))}
-        </div>
-      </main>
-    </div>
-    <Footer/>
+          {/* Kartu Kecil dan Pagination */}
+          <div className="flex flex-col w-full lg:w-2/5">
+            {/* Kartu Kecil */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 mt-3 gap-4">
+              {currentItems.slice(1).map((data, index) => (
+                <Card key={index} {...data} />
+              ))}
+            </div>
+
+            {/* Pagination */}
+            <div className="mt-4 flex justify-center gap-2">
+              <button
+                className={`bg-gray-200 text-black py-2 px-4 rounded-lg shadow-md ${
+                  currentPage === 1
+                    ? "opacity-50 cursor-not-allowed"
+                    : "hover:bg-gray-300"
+                }`}
+                onClick={prevPage}
+              >
+                ⇦ Sebelumnya
+              </button>
+              <button
+                className={`bg-gray-200 text-black py-2 px-4 rounded-lg shadow-md ${
+                  currentPage === Math.ceil(cardData.length / itemsPerPage)
+                    ? "opacity-50 cursor-not-allowed"
+                    : "hover:bg-gray-300"
+                }`}
+                onClick={nextPage}
+                disabled={
+                  currentPage === Math.ceil(cardData.length / itemsPerPage)
+                }
+              >
+                Selanjutnya ⇨
+              </button>
+            </div>
+          </div>
+        </main>
+      </div>
+      <Footer />
     </>
   );
 };
